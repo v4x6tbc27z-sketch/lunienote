37d553b2bb73332fc1b7da712d6cf2bc080f1e59
khunthanitt <khunthanit98@gmail.com>
2026-08-18 08:26:32 +0700

    deploy: LunieNote web v3.1.1 (search-to-jump in map location picker)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2133349369" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3387427536" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
