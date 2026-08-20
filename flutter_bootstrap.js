39ace9566d58d45cba9d5de1cb8237c5004253ea
khunthanitt <khunthanitt@MacBook-Air-khxng-khunthanitt.local>
2026-08-20 14:57:12 +0700

    deploy: LunieNote web v3.11 (fix deleted events reviving via sync)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3127969475" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "177673126" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
