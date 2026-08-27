ab654ffb3f25c9199cee33c46edcc9bc38ad9caa
khunthanitt <khunthanitt@MacBook-Air-khxng-khunthanitt.local>
2026-08-27 08:10:45 +0700

    deploy: LunieNote web v3.18 (fix invisible header logo)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1516959835" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "853686526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
